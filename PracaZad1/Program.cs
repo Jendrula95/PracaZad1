1007f6366cd92b11510347481b0bde9271ef0010
Jendrula95 <Jendrula95@hotmail.com>
2024-06-12 18:59:25 +0200

    Dodaj pliki projektów.

---
+++ PracaZad1/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student("student@email.com");
+        Kontrahent kontrahent = new Kontrahent("kontrahent@email.com");
+        Urzad urzad = new Urzad("urzad@email.com");
+
+        EmailSender emailSender = new EmailSender();
+
+        emailSender.SendEmail(student);
+        emailSender.SendEmail(kontrahent);
+        emailSender.SendEmail(urzad);
+
+      
+    }
+}
+public interface IEmail
+{
+    string Email { get; set; }
+}
+
+public class Student : IEmail
+{
+    public string Email { get; set; }
+
+    public Student(string email)
+    {
+        Email = email;
+    }
+}
+
+public class Kontrahent : IEmail
+{
+    public string Email { get; set; }
+
+    public Kontrahent(string email)
+    {
+        Email = email;
+    }
+}
+
+public class Urzad : IEmail
+{
+    public string Email { get; set; }
+
+    public Urzad(string email)
+    {
+        Email = email;
+    }
+}
+public class EmailSender
+{
+    public void SendEmail(IEmail contact)
+    {
+        Console.WriteLine($"Email wysłano do : {contact.Email}");
+    }
+}
+
+
+
